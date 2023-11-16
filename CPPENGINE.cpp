ab3c66b82899c18e1476d693e3ff193f1e1b8ab8
MrStark7797 <starkiewill@gmail.com>
2023-11-16 20:02:52 +0000

    Checking Checkboard

--- CPPENGINE.cpp
+++ CPPENGINE.cpp
@@ -12,26 +12,17 @@ using namespace std;
 int main() {
     AllInit();
     
-    string FEN1_S = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1";
-    string FEN2_S = "rnbqkbnr/pp1ppppp/2p5/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq c6 0 2";
-    string FEN3_S = "rnbqkbnr/pppppppp/2p5/8/4P3/5N2/PPPP1PPP/RNBQKBNR b KQkq - 1 2";
+    string FEN1_S = "8/8/2R2pk1/P5p1/8/5PK1/r4BP1/5b2 b - - 5 52;";
+    
 
 
     S_BOARD board[1];
     char* FEN1 = FEN1_S.data();
-    char* FEN2 = FEN2_S.data();
-    char* FEN3 = FEN3_S.data();
-
-    ParseFen(START_FEN, board);
-    PrintBoard(board);
+    
 
     ParseFen(FEN1, board);
     PrintBoard(board);
 
-    ParseFen(FEN2, board);
-    PrintBoard(board);
-
-    ParseFen(FEN3, board);
-    PrintBoard(board);
+    ASSERT(CheckBoard(board));
 
 }
